317829efa10adaeaf57e572707f8a99a26c44bb0
herreramaxi <herreramaxi@gmail.com>
2025-05-25 15:33:39 +0100

    add Flask templates

--- flaskApp.py
+++ flaskApp.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from joblib import load
 import pandas as pd
 import numpy as np
@@ -8,17 +8,37 @@ app=Flask(__name__)
 model=load("SpamModel.pkl")
 version = "1.0.0"
 
-
-@app.route('/')
+@app.route('/version')
 def welcome():
     return f"Welcome to Spam Prediction App (version {version})"
 
+@app.route("/")
+def home():
+    return render_template("index.html", version=version)
+
 @app.route('/predict')
 def predict_spam():
-    experience=request.args.get('text')
-    prediction=model.predict([[experience]])
+    text=request.args.get('text')
+    prediction=model.predict([text])
    
     return "The predicted value is" + str(prediction)
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    text = request.form["text"]    
+    prediction=model.predict([text])
+    prediction_text = "Not Spam"
+
+    if prediction == 0:
+        prediction_text = "Not Spam"
+    elif prediction == 1:
+        prediction_text = "Spam"
+    else:
+        prediction_text = "Unknown"   
+
+    formatted_prediction = f"The predicted value is: {prediction_text}"
+
+    return render_template("result.html", prediction=formatted_prediction)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
